a03b81c71998e7a3a402935a4a5119a98c58f36b
shin <sshoony95@gmail.com>
2023-03-30 16:20:51 -0400

    Clear input field on submit

--- src/popup/Popup.js
+++ src/popup/Popup.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
 import "./Popup.css";
-import { useSelector, useDispatch } from 'react-redux'
-import { setCommand } from './popupSlice'
+import { useSelector, useDispatch } from "react-redux";
+import { setCommand } from "./popupSlice";
 
 const Popup = () => {
   const command = useSelector((state) => state.popup.command);
   const dispatch = useDispatch();
+  const [inputValue, setInputValue] = useState("");
   useEffect(() => {
     const messageToContent = async () => {
       const [tab] = await chrome.tabs.query({
@@ -15,16 +16,18 @@ const Popup = () => {
       chrome.tabs.sendMessage(
         tab.id,
         //   {q: "setCommand", command},
-        { q: "setCommand", command },
+        { q: "setCommand", command }
         // (res) => setPlaceholder(res.command)
       );
     };
     messageToContent();
   }, [command]);
 
+  const handleChange = (e) => setInputValue(e.currentTarget.value);
   const handleFormSubmit = async (e) => {
     e.preventDefault();
     console.log(e);
+    setInputValue("");
     dispatch(setCommand(e.target[0].value));
     //send a message to content to set the commend with the new input
   };
@@ -33,7 +36,14 @@ const Popup = () => {
     <div>
       <form onSubmit={handleFormSubmit} id="form">
         <label>Enter a command for text processing</label>
-        <input type="text" name="command" id="command" placeholder={command} />
+        <input
+          type="text"
+          name="command"
+          id="command"
+          onChange={handleChange}
+          placeholder={command}
+          value={inputValue}
+        />
       </form>
       <div>
         <h1>This is a popup section</h1>
