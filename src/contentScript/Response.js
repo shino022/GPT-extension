d9630c01fae0e9ffe5187ba31fd999e291cada3c
shin <sshoony95@gmail.com>
2023-04-18 17:19:50 -0400

    Add button disabled functionality on copy

--- src/contentScript/Response.js
+++ src/contentScript/Response.js
@@ -1,29 +1,37 @@
 import React, { useCallback, useState } from "react";
 import { IconCopy } from "@tabler/icons-react";
 const Response = ({ response }) => {
+  const [showCopyIconText, setShowCopyIconText] = useState(false);
+  const [copyIconText, setCopyIconText] = useState('Copy');
+  const [hasCopied, setHasCopied] = useState(false);
   const onClickCopyButton = useCallback(
     (e) => {
       console.log(response);
       navigator.clipboard.writeText(response);
+      setCopyIconText('Copied');
+      setHasCopied(true);
     },
     [response]
   );
-  const [showCopyIconText, setShowCopyIconText] = useState(false);
+
   const onHover = () => {
     setShowCopyIconText(true);
   };
+
   const onLeave = () => {
     setShowCopyIconText(false);
   };
+
   return (
     <div>
       <IconCopy
+        className={hasCopied && "btn-disabled"}
         id="copy-icon"
         onMouseEnter={onHover}
         onMouseLeave={onLeave}
         onClick={onClickCopyButton}
       />
-      {showCopyIconText && <div id="copy-icon-text">Copy</div>}
+      {showCopyIconText && <div id="copy-icon-text">{copyIconText}</div>}
       <div id="response-text">
         {response}
       </div>
